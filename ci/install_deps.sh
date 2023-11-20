f2f60c146c4bfe3c2e932b6af0403e51975de61f
tbrekalo <t@tbrekalo.xyz>
2023-11-20 07:26:01 +0000

    spoa-upgrade: bumping minimum cmake version

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -3,11 +3,11 @@ set -eou pipefail
 
 apt update && apt install -y git zlib1g-dev curl ninja-build
 
-VERSION="3.13.4"
+VERSION="3.16.5"
 MIRROR_URL="https://github.com/Kitware/CMake/releases/download/v$VERSION"
 DOWNLOAD="cmake-$VERSION-linux-x86_64.sh"
 DOWNLOAD_FILE="/tmp/cmake.sh"
 
 curl -Ls "${MIRROR_URL}/${DOWNLOAD}" --output "${DOWNLOAD_FILE}"
 bash "${DOWNLOAD_FILE}" --skip-license --prefix=/usr/local --exclude-subdir
-which cmake
+cmake --version
